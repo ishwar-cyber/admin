34dc0448c5be9fc2d8ab26b5ee9a1526cb906fc3
ishwar-cyber <panditishwar21@gmail.com>
2026-03-07 07:19:29 +0530

    change base url to railway server

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
-    BASE_URL: 'https://shoppyness-backend.onrender.com/api/v1',
+    // BASE_URL: 'https://shoppyness-backend.onrender.com/api/v1',
+    BASE_URL: 'https://shoppyness-application.up.railway.app/api/v1',
     // BASE_URL: 'http://localhost:8000/api/v1',
     production: false,
 };
